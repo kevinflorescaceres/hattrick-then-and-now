8fa6ab83a05f9e89a425f57ab33cfd68c98eb983
Kevin Flores Cáceres <kevin.flores.c@gmail.com>
2024-07-16 01:12:12 -0400

    first commit: create project, api app, postgresql database, decouple envs

---
+++ hattrick_then_and_now/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hattrick_then_and_now project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hattrick_then_and_now.settings')
+
+application = get_wsgi_application()
